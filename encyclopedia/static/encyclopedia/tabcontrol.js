74a683509985d86fecc943b58fee1719335123f9
USERPC\USER <royalfamily89@gmail.com>
2018-01-21 00:46:43 +0900

    20180121 . ajax(Asyncrous javascript and xml) 적용

--- encyclopedia/static/encyclopedia/tabcontrol.js
+++ encyclopedia/static/encyclopedia/tabcontrol.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
   for(i = 0; i < $('#iframecount').val(); i++) {
     selector = "#listNum";
     selector = selector.concat(i);
-
-    $(selector).attr('src', '/static/encyclopedia/sourcestorage/codeinventory/'+i);
+    var address = '/static/encyclopedia/sourcestorage/codeinventory/'+i+'.html';
+    $(selector).attr('src', address);
     //files structure setting 필요
   }
 
@@ -19,19 +19,30 @@ $(document).ready(function(){
   });
 
   $('#nav-insert-tab').click(function(){
+    console.log("hello world");
       $('#layoutTitle').slideUp("slow");
   });
 
   $("form").submit(function() {
       var id= $("input[type=submit][clicked=true]").attr("id");
-      if(id == 'savebutton'){
+      console.log("this is public service announcement");
+      //if(id == 'savebutton'){
         $('#selectorID').val('makeFILE');
-      }
+      //}
    });
 
-   $("form input[type=submit]").click(function() {
-       $("input[type=submit]", $(this).parents("form")).removeAttr("clicked");
-       $(this).attr("clicked", "true");
-   });
+   $("#transferbutton").click(function(){
+     var formData = $("#codeform").serialize();
+     console.log("this section is executed.");
+     $('#selectorID').val('makeDEMO');
 
+     $.ajax({
+         type : "POST",
+         url : "http://localhost:8000/encyclopedia/upload",
+         data : formData,
+         success:function(data){
+           $('#tmpresult').attr('src','/static/encyclopedia/sourcestorage/hello.html');
+         }
+     });
+   });
 });
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,6 +3,7 @@ from .models import Content, Service
 from .forms import SourceForm
 import logging
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.core.files import File
 
 # Create your views here.
@@ -10,18 +11,18 @@ def index(request):
 
     logger = logging.getLogger(__name__)
     source_list = Content.objects.order_by('id')
-    menu_list = Service.objects.order_by('id')
     form = SourceForm()
 
+    #file writing.
     for i in range(0, source_list.count()):
-        logger.error(i)
-        with open('C:/Users/USER/Documents/DjangoWebProject/encyclopedia/static/encyclopedia/sourcestorage/codeinventory/'+str(i), 'w+') as f:
+        with open('C:/Users/USER/Documents/DjangoWebProject/encyclopedia/static/encyclopedia/sourcestorage/codeinventory/'+str(i)+".html", 'w+') as f:
             myfile = File(f)
             myfile.write('<!DOCTYPE html><html>'+'<head>'+'<style>'+source_list[i].csscontents+'</style>'+'</head>'+'<body>'+source_list[i].htmlcontents+'</body>'+'<script>'+source_list[i].javascriptcontents+'</script></html>')
             myfile.closed
             f.closed
 
-    context = {'source_list':source_list,'menu_list':menu_list, 'form':form, 'index':0}
+    #context = {'source_list':source_list,'menu_list':menu_list, 'form':form, 'index':0 }
+    context = {'source_list':source_list,'menu_list':["list","insert","search"], 'form':form, 'index':0 }
     return render(request, 'encyclopedia/maintab.html', context)
 
 # file upload.
@@ -29,32 +30,40 @@ def upload_file(request):
 
     logger = logging.getLogger(__name__)
     source_list = Content.objects.order_by('id')
-    menu_list = Service.objects.order_by('id')
 
     if request.method == 'POST':
-            #if fileform is '1' then...
 
-            logger.error("Uploadfileform and form. is Post is executing")
+            logger.error("Uploadfileform and form. Post is executing")
             fileform = SourceForm(request.POST)
-
+            logger.error(request.POST)
             if(fileform.is_valid()):
                 if(request.POST['selector'] == 'makeDEMO'):
+                    logger.error("makeDEMO is executing.")
                     with open('C:/Users/USER/Documents/DjangoWebProject/encyclopedia/static/encyclopedia/sourcestorage/hello.html', 'w+') as f:
                         myfile = File(f)
                         logger.error(fileform.cleaned_data['csscontents'])
                         myfile.write('<!DOCTYPE html><html>'+'<head>'+'<style>'+fileform.cleaned_data['csscontents']+'</style>'+'</head>'+'<body>'+fileform.cleaned_data['htmlcontents']+'</body>'+'<script>'+fileform.cleaned_data['javascriptcontents']+'</script></html>')
                         myfile.closed
                         f.closed
+                    return HttpResponse("success")
 
                 else:
+                    with open('C:/Users/USER/Documents/DjangoWebProject/encyclopedia/static/encyclopedia/sourcestorage/codeinventory/'+str(source_list.count())+'.html', 'w+') as f:
+                        myfile = File(f)
+                        logger.error(fileform.cleaned_data['csscontents'])
+                        myfile.write('<!DOCTYPE html><html>'+'<head>'+'<style>'+fileform.cleaned_data['csscontents']+'</style>'+'</head>'+'<body>'+fileform.cleaned_data['htmlcontents']+'</body>'+'<script>'+fileform.cleaned_data['javascriptcontents']+'</script></html>')
+                        myfile.closed
+                        f.closed
+                    logger.error("file save is executing")
                     post = fileform.save(commit=True)
                     post.save()
 
     else:
-        fileform = SourceForm()
         logger.error("request is not post and uploadfileform is not valid.")
+        fileform = SourceForm()
 
-    context = {'source_list':source_list,'menu_list':menu_list, 'form':fileform, 'index':1}
+
+    context = {'source_list':source_list,'menu_list':["list","insert","search"], 'form':fileform, 'index':1}
     return render(request, 'encyclopedia/maintab.html', context) # to do configure to current tab.
 
 # define code page
